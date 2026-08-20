ce0cc4a213152f217f4edd7553374d91e7c8899b
luizatrigueiro <lulu.study.2005@gmail.com>
2026-08-20 14:18:39 -0300

    chore(upgrade-airflow-3): atualizar dependências para Airflow 3

--- airflow/helpers/postgres_helpers.py
+++ airflow/helpers/postgres_helpers.py
@@ -1,24 +1,22 @@
 import logging
+
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 
 def get_postgres_conn(data_base_name: str = "postgres_default") -> str:
     try:
         hook = PostgresHook(postgres_conn_id=data_base_name)
-        conn = hook.get_conn()
-        try:
-            schema = conn.info.dbname
-            logging.info(
-                f"[postgres_helpers] Obtained PostgreSQL connection: "
-                f"dbname={schema}, user={conn.info.user},"
-                f"host={conn.info.host}, port={conn.info.port}"
-            )
-            return (
-                f"dbname={schema} user={conn.info.user} password={conn.info.password} "
-                f"host={conn.info.host} port={conn.info.port}"
-            )
-        finally:
-            conn.close()
+        connection = hook.get_connection(data_base_name)
+        schema = connection.schema
+        logging.info(
+            f"[postgres_helpers] Obtained PostgreSQL connection: "
+            f"dbname={schema}, user={connection.login},"
+            f"host={connection.host}, port={connection.port}"
+        )
+        return (
+            f"dbname={schema} user={connection.login} password={connection.password} "
+            f"host={connection.host} port={connection.port}"
+        )
     except Exception as e:
         logging.error(f"Failed to obtain PostgreSQL connection: {e}")
         raise
